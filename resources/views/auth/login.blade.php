7a128acd8c4f30aeed09d9b7f4f5c64334d23498
afauzansw <afsw.fauzan33142@gmail.com>
2021-10-22 15:50:10 +0700

    custom login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,123 @@
 <x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+{{--    <x-jet-authentication-card>--}}
+{{--        <x-slot name="logo">--}}
+{{--            <x-jet-authentication-card-logo />--}}
+{{--        </x-slot>--}}
 
-        <x-jet-validation-errors class="mb-4" />
+{{--        <x-jet-validation-errors class="mb-4" />--}}
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
-            </div>
-        @endif
+{{--        @if (session('status'))--}}
+{{--            <div class="mb-4 font-medium text-sm text-green-600">--}}
+{{--                {{ session('status') }}--}}
+{{--            </div>--}}
+{{--        @endif--}}
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+{{--        <form method="POST" action="{{ route('login') }}">--}}
+{{--            @csrf--}}
 
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
+{{--            <div>--}}
+{{--                <x-jet-label for="email" value="{{ __('Email') }}" />--}}
+{{--                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />--}}
+{{--            </div>--}}
 
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
+{{--            <div class="mt-4">--}}
+{{--                <x-jet-label for="password" value="{{ __('Password') }}" />--}}
+{{--                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />--}}
+{{--            </div>--}}
 
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
+{{--            <div class="block mt-4">--}}
+{{--                <label for="remember_me" class="flex items-center">--}}
+{{--                    <x-jet-checkbox id="remember_me" name="remember" />--}}
+{{--                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>--}}
+{{--                </label>--}}
+{{--            </div>--}}
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
+{{--            <div class="flex items-center justify-end mt-4">--}}
+{{--                @if (Route::has('password.request'))--}}
+{{--                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">--}}
+{{--                        {{ __('Forgot your password?') }}--}}
+{{--                    </a>--}}
+{{--                @endif--}}
+
+{{--                <x-jet-button class="ml-4">--}}
+{{--                    {{ __('Log in') }}--}}
+{{--                </x-jet-button>--}}
+{{--            </div>--}}
+{{--        </form>--}}
+{{--    </x-jet-authentication-card>--}}
+    <div class="flex flex-col overflow-y-auto md:flex-row">
+        <div class="h-32 md:h-auto md:w-1/2">
+            <img
+                aria-hidden="true"
+                class="object-cover w-full h-full dark:hidden"
+                src="../assets/img/login-office.jpeg"
+                alt="Office"
+            />
+            <img
+                aria-hidden="true"
+                class="hidden object-cover w-full h-full dark:block"
+                src="../assets/img/login-office-dark.jpeg"
+                alt="Office"
+            />
+        </div>
+        <div class="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
+            <div class="w-full">
+                <h1
+                    class="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200"
+                >
+                    Login
+                </h1>
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <label class="block text-sm">
+                        <span class="text-gray-700 dark:text-gray-400">Email</span>
+                        <input
+                            class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                            placeholder="Jane Doe"
+                            type="email"
+                            id="email"
+                            name="email"
+                        />
+                    </label>
+                    <label class="block mt-4 text-sm">
+                        <span class="text-gray-700 dark:text-gray-400">Password</span>
+                        <input
+                            class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                            placeholder="***************"
+                            type="password"
+                            id="password"
+                            name="password"
+                        />
+                    </label>
+
+                    <!-- You should use a button here, as the anchor is only used for the example  -->
+                    <button
+                        class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
+                        type="submit"
+                    >
+                        Log in
+                    </button>
+                </form>
+
+                <hr class="my-8" />
+
+                <p class="mt-4">
+                    <a
+                        class="text-sm font-medium text-purple-600 dark:text-purple-400 hover:underline"
+                        href="./forgot-password.html"
+                    >
+                        Forgot your password?
                     </a>
-                @endif
-
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-jet-button>
+                </p>
+                <p class="mt-1">
+                    <a
+                        class="text-sm font-medium text-purple-600 dark:text-purple-400 hover:underline"
+                        href="./create-account.html"
+                    >
+                        Create account
+                    </a>
+                </p>
             </div>
-        </form>
-    </x-jet-authentication-card>
+        </div>
+    </div>
 </x-guest-layout>
