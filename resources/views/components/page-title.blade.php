4b869212e01f967211a1d450db6c27a72f76ac57
afauzansw <afsw.fauzan33142@gmail.com>
2021-10-22 15:35:26 +0700

    customize dashboard template

---
+++ resources/views/components/page-title.blade.php
@@ -0,0 +1,3 @@
+<h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+    {{ $title }}
+</h2>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,46 +1,91 @@
+{{--<!DOCTYPE html>--}}
+{{--<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">--}}
+{{--    <head>--}}
+{{--        <meta charset="utf-8">--}}
+{{--        <meta name="viewport" content="width=device-width, initial-scale=1">--}}
+{{--        <meta name="csrf-token" content="{{ csrf_token() }}">--}}
+
+{{--        <title>{{ config('app.name', 'Laravel') }}</title>--}}
+
+{{--        <!-- Fonts -->--}}
+{{--        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">--}}
+
+{{--        <!-- Styles -->--}}
+{{--        <link rel="stylesheet" href="{{ mix('css/app.css') }}">--}}
+
+{{--        @livewireStyles--}}
+
+{{--        <!-- Scripts -->--}}
+{{--        <script src="{{ mix('js/app.js') }}" defer></script>--}}
+{{--    </head>--}}
+{{--    <body class="font-sans antialiased">--}}
+{{--        <x-jet-banner />--}}
+
+{{--        <div class="min-h-screen bg-gray-100">--}}
+{{--            @livewire('navigation-menu')--}}
+
+{{--            <!-- Page Heading -->--}}
+{{--            @if (isset($header))--}}
+{{--                <header class="bg-white shadow">--}}
+{{--                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">--}}
+{{--                        {{ $header }}--}}
+{{--                    </div>--}}
+{{--                </header>--}}
+{{--            @endif--}}
+
+{{--            <!-- Page Content -->--}}
+{{--            <main>--}}
+{{--                {{ $slot }}--}}
+{{--            </main>--}}
+{{--        </div>--}}
+
+{{--        @stack('modals')--}}
+
+{{--        @livewireScripts--}}
+{{--    </body>--}}
+{{--</html>--}}
+
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-
-        @livewireStyles
-
-        <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
-    </head>
-    <body class="font-sans antialiased">
-        <x-jet-banner />
-
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-
-        @stack('modals')
-
-        @livewireScripts
-    </body>
+<html :class="{ 'theme-dark': dark }" x-data="data()" lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Windmill Dashboard</title>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+        rel="stylesheet"
+    />
+    <link rel="stylesheet" href="{{ asset('assets/css/tailwind.output.css') }}" />
+    <script
+        src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"
+        defer
+    ></script>
+    <script src="{{ asset('assets/js/init-alpine.js') }}"></script>
+    <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css"
+    />
+    <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"
+        defer
+    ></script>
+    <script src="{{ asset('assets/js/charts-lines.js') }}" defer></script>
+    <script src="{{ asset('assets/js/charts-pie.js') }}" defer></script>
+</head>
+<body>
+<div
+    class="flex h-screen bg-gray-50 dark:bg-gray-900"
+    :class="{ 'overflow-hidden': isSideMenuOpen }"
+>
+    <x-sidebar/>
+    <div class="flex flex-col flex-1 w-full">
+        <x-header/>
+        <main class="h-full overflow-y-auto">
+            {{ $slot }}
+        </main>
+    </div>
+</div>
+@livewireScripts
+</body>
 </html>
+
